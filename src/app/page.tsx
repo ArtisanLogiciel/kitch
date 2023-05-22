b615492fabaf650d891fc7d4ada49da82e7d9947
Nelson <nelson.vico@gmail.com>
2023-05-22 14:24:31 +0200

    v3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,69 +1,7 @@
-import BarreDeRecherche from "../../Components/BarreDeRecherche"
-import NavigLive from "../../Components/NaviguationLive"
+import App from "./Inter"
 
-export default function Home() {
-  
+export default function Home() {  
   return (
-    <>
-      <BarreDeRecherche />
-      <NavigLive />
-      <div style={{
-        width: '78%',
-        height: '100%',
-        border: '1px solid yellow', 
-        marginTop: '10vh',
-        marginLeft: '20vw',
-        }}>
-          <p>Contenu</p>
-          <p>Conteunu</p>
-          <p>Conteunu</p>
-          <p>Conteunu</p>
-          <p>Conteunu</p>
-          <p>Conteunu</p>
-          <p>Conteunu</p>
-          <p>Conteunu</p>
-          <p>Contenu</p>
-          <p>Conteunu</p>
-          <p>Conteunu</p>
-          <p>Conteunu</p>
-          <p>Conteunu</p>
-          <p>Conteunu</p>
-          <p>Conteunu</p>
-          <p>Conteunu</p>
-          <p>Contenu</p>
-          <p>Conteunu</p>
-          <p>Conteunu</p>
-          <p>Conteunu</p>
-          <p>Conteunu</p>
-          <p>Conteunu</p>
-          <p>Conteunu</p>
-          <p>Conteunu</p>
-          <p>Contenu</p>
-          <p>Conteunu</p>
-          <p>Conteunu</p>
-          <p>Conteunu</p>
-          <p>Conteunu</p>
-          <p>Conteunu</p>
-          <p>Conteunu</p>
-          <p>Conteunu</p>
-          <p>Contenu</p>
-          <p>Conteunu</p>
-          <p>Conteunu</p>
-          <p>Conteunu</p>
-          <p>Conteunu</p>
-          <p>Conteunu</p>
-          <p>Conteunu</p>
-          <p>Conteunu</p>
-          <p>Contenu</p>
-          <p>Conteunu</p>
-          <p>Conteunu</p>
-          <p>Conteunu</p>
-          <p>Conteunu</p>
-          <p>Conteunu</p>
-          <p>Conteunu</p>
-          <p>Conteunu</p>
-        </div>
-    </>
-
+    <App />
   )
 }
