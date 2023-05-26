0dde0bff92b50ee094344127b7a5c2271512a45b
antonin michel <a.copiercoller@gmail.com>
2023-05-26 20:16:21 +0200

    copie de nelson

---
+++ src/app/api/twitch/token/route.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from "next/server";
+
+
+export async function POST(){
+    const options = {
+        method: 'POST',
+         headers: {
+    'Content-Type': 'application/json',
+  },
+        body: JSON.stringify({
+            client_id: process.env.DB_CLIENT || '',
+            client_secret: process.env.DB_KEY_SECRET || '',
+            grant_type: 'client_credentials',
+          }),
+    };
+    const res = await fetch('https://id.twitch.tv/oauth2/token', options)
+
+    const twitch = await res.json()
+    return NextResponse.json(twitch)
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,7 @@
-import Image from "next/image";
-import NavBar from "./components/NavBar";
-import ApiConnexion from "./components/ApiConnexion";
-import Post from "./components/ApiConnexionNelson";
-import TwitchStreamer from "./components/TwitchStreamer";
-import TwitchComponent from "./components/TwitchComponent";
+import App from "./Inter"
 
-export default function Home() {
+export default function Home() {  
   return (
-    <div>
-      <NavBar />
-      <TwitchStreamer streamerName="JeuSerieux" />
-      <TwitchComponent />
-      {/* <ApiConnexion /> */}
-      {/* <Post /> */}
-    </div>
-  );
+    <App />
+  )
 }
