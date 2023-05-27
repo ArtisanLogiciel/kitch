75e06671ca21d148cbd8d17fb508489a1c8bd77f
Nelson <nelson.vico@gmail.com>
2023-05-27 19:01:28 +0200

    v7

--- Components/FetchSearch.tsx
+++ Components/FetchSearch.tsx
@@ -19,4 +19,13 @@ export async function GetUsers(donnes: any) {
         console.log(error.message)
     }   
 }
+export async function fetchStreams(donnes : string){
+    try{
+        const response = await fetch(`https://api.twitch.tv/helix/streams?user_login=${donnes}`);
+        const dataStream = await response.json();
+        return dataStream?.data
+    }catch(error: any){
+        console.log(error.message)
+    }
+}
 
