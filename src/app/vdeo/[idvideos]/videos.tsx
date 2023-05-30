ad713c470ff4ef5c228d2dcbad6a257671370d46
Nelson <nelson.vico@gmail.com>
2023-05-30 21:16:36 +0200

    version8

--- src/app/vdeo/[idvideos]/videos.tsx
+++ src/app/vdeo/[idvideos]/videos.tsx
@@ -5,9 +5,6 @@ import NavigLive from '../../../../Components/NaviguationLive';
 import { useParams } from 'next/navigation';
 import { default as _ReactPlayer } from 'react-player/lazy';
 import { ReactPlayerProps } from "react-player/types/lib";
-import { fetchStreams } from '../../../../Components/FetchSearch';
-import { Image } from '../../../../Components/UsefulsComponents';
-
 
 
 const ReactPlayer = _ReactPlayer as unknown as React.FC<ReactPlayerProps>;
@@ -32,32 +29,13 @@ export default function Home(){
   <>
       <BarreDeRecherche />
       <NavigLive />
-      <div className="WidthContainer h-full border-2 border-solid border-b-yellow-300 marginLeftVw">
-        <div className='VdeoContainer'>
-          <div className='Blockvideos'>
-            <ReactPlayer url={`https://www.twitch.tv/${params.idvideos}`} className='react-player' controls width={'100%'} height={'70vh'}/>
-              <div style={{width: '100%'}}>
-                  <div style={{width: '25%'}}>
-                      {/*<img  src={Image(dataVDO?.thumbnail_url, '50', '50')} style={{borderRadius: '50%', width: '50px', height: '50px'}}></img>*/}
-                      <p>Image</p>
-                  </div>
-                  <div style={{width: '40%'}}>
-                        <h2>{dataVDO?.user_name}</h2>
-                        <p>{dataVDO?.title}</p>
-                        <p>{dataVDO?.game_name}</p>
-                  </div>
-                  <div>
-                      <button>Suivre</button>
-                      <button>S'abonner</button>
-                  </div>
-                  
-              </div>
-          </div>
-          
-           
-        </div>  
-      </div>
-  </>
+      <div className="ContenuPrincipale">
+        <div>
+        <ReactPlayer url={`https://www.twitch.tv/${params.idvideos}`} className='react-player' controls />
+          Chemin videos/ IdVIdeos
+        </div>
+        </div>
+    </>
           
 
     )
