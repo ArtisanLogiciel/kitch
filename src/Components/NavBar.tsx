9137d44a37afc46e16f76197c84ccae63cf38d26
antonin michel <a.copiercoller@gmail.com>
2023-05-31 22:12:16 +0200

    commit

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -81,10 +81,10 @@ export default function NavBar() {
       flexWrap: 'nowrap',      
       }}>
      
-     <svg width="3.5vw" height="3.5vh" viewBox="0 0 20 20"><path fillRule="evenodd" d="M13.798 10.456 10 6.657l-3.798 3.799L4 8.805V13h12V8.805l-2.202 1.65zM18 5v8a2 2 0 0 1-2 2H4a2.002 2.002 0 0 1-2-2V5l4 3 4-4 4 4 4-3z" clipRule="evenodd"></path></svg>
+     <svg width="3.5%" height="3.5%" viewBox="0 0 20 20"><path fillRule="evenodd" d="M13.798 10.456 10 6.657l-3.798 3.799L4 8.805V13h12V8.805l-2.202 1.65zM18 5v8a2 2 0 0 1-2 2H4a2.002 2.002 0 0 1-2-2V5l4 3 4-4 4 4 4-3z" clipRule="evenodd"></path></svg>
       <button className="BTNLogin">Se Connecter</button>
       <button className="BTNSub"> S{"'"}inscrire</button>
-      <svg width="3.5vw" height="3.5vh" viewBox="0 0 20 20"><path fillRule="evenodd" d="M5 7a5 5 0 1 1 6.192 4.857A2 2 0 0 0 13 13h1a3 3 0 0 1 3 3v2h-2v-2a1 1 0 0 0-1-1h-1a3.99 3.99 0 0 1-3-1.354A3.99 3.99 0 0 1 7 15H6a1 1 0 0 0-1 1v2H3v-2a3 3 0 0 1 3-3h1a2 2 0 0 0 1.808-1.143A5.002 5.002 0 0 1 5 7zm5 3a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" clipRule="evenodd"></path></svg>
+      <svg width="3.5%" height="3.5%" viewBox="0 0 20 20"><path fillRule="evenodd" d="M5 7a5 5 0 1 1 6.192 4.857A2 2 0 0 0 13 13h1a3 3 0 0 1 3 3v2h-2v-2a1 1 0 0 0-1-1h-1a3.99 3.99 0 0 1-3-1.354A3.99 3.99 0 0 1 7 15H6a1 1 0 0 0-1 1v2H3v-2a3 3 0 0 1 3-3h1a2 2 0 0 0 1.808-1.143A5.002 5.002 0 0 1 5 7zm5 3a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" clipRule="evenodd"></path></svg>
       </div>
     </AppBar>
   );
