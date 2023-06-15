0902a107992821ac199e4127a6c886132d1d3dd8
antonin michel <a.copiercoller@gmail.com>
2023-06-15 19:01:57 +0200

    modification channel

--- src/app/[userLogin]/page.tsx
+++ src/app/[userLogin]/page.tsx
@@ -10,38 +10,27 @@ import { ReactPlayerProps } from "react-player/types/lib";
 import { MainAppBar } from "@/Components/MainAppBar";
 //import NavigLive from "../../Components/NaviguationLive";
 import NavigLive from "@/Components/NaviguationLive";
-import Channel from '@/Components/Channel'
-import Chat from '@/Components/Chat'
+import Channel from "@/Components/Channel";
+import Chat from "@/Components/Chat";
 
 const ReactPlayer = _ReactPlayer as unknown as React.FC<ReactPlayerProps>;
 
-
 export default function Stream() {
-
   const params = useParams();
 
   console.log("********([userLogin]/page.tsx) => params : ", params);
 
   const userLogin = params.userLogin;
 
-  console.log("---2-([userLogin]/page.tsx) Au clic sur 1 chaine (<Link href={`/${channelName?.user_login}`}) : ", userLogin);
+  console.log(
+    "---2-([userLogin]/page.tsx) Au clic sur 1 chaine (<Link href={`/${channelName?.user_login}`}) : ",
+    userLogin
+  );
 
   return (
-    <>     
-    <><Channel userLogin={userLogin} />
-      <Chat userLogin={userLogin} /> </>
-
-
-      {/* <div className="ContenuPrincipale">
-        <div>
-          <ReactPlayer
-            url={`https://www.twitch.tv/${params.idvideos}`}
-            className="react-player"
-            controls
-          />
-          Chemin videos/ IdVIdeos
-        </div>
-      </div> */}
+    <>
+      <Channel userLogin={userLogin} />
+      <Chat userLogin={userLogin} />
     </>
   );
 }
