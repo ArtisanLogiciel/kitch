63cf02ffd45035531ed54eeaea5361b4de81423e
antonin michel <a.copiercoller@gmail.com>
2023-06-30 21:40:24 +0200

    tentative résolution de conflits #5

--- src/app/[userLogin]/page.tsx
+++ src/app/[userLogin]/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import React from "react";
-
-import { useParams, useRouter, useSearchParams } from "next/navigation";
+// import { IntrinsicAttributes } from "react";
+// import { UserLoginProps } from "@/types/userLogin";
+import { useParams, useSearchParams } from "next/navigation";
+import { useRouter } from "next/router";
 
 import { default as _ReactPlayer } from "react-player/lazy";
 import { ReactPlayerProps } from "react-player/types/lib";
@@ -11,24 +13,28 @@ import { ReactPlayerProps } from "react-player/types/lib";
 import Channel from "@/Components/Channel";
 import Chat from "@/Components/Chat";
 
-
+// interface StreamProps {
+//   viewer: IntrinsicAttributes | UserLoginProps;
+//   timeSecondes: number;
+// }
 export default function Stream() {
   const searchParams = useSearchParams();
-  const viewer = Number(searchParams.get("viewer"))
+  const viewer: number = Number(searchParams.get("viewer"))
   const timestamp = searchParams.get("time")
-  const timestampNb = Number(timestamp)
+  const timestampNb: number = Number(timestamp)
 
   const timestampNow = Date.parse(new Date().toString())  // car le timestamp est en STRING (pourtant passé en NUMBER dans le LINK) !!!???
 
   // On calcule le temps écoulé en secondes depuis le début de la vidéo
   const timeSecondes = (timestampNow - timestampNb) / 1000
-
+  const router = useRouter();
+  const userLogin = router.query.userLogin as string;
   const [activeChat, setActiveChat] = React.useState<boolean>(true);
-  console.log("******activeChat******* : ",activeChat);
+  // console.log("******activeChat******* : ",activeChat);
 
   return (
     <div className={activeChat ? "grid grid-cols-[80%_20%] w-full h-full " : "grid grid-cols-[100%_0%] w-full h-full "} >
-        <Channel viewer={viewer} timeSecondes={timeSecondes} />
+        <Channel userLogin={userLogin} />
         <div className={!activeChat ? "ml-5 " : ""}><Chat activeChat={activeChat} setActiveChat={setActiveChat} /></div>
     </div>
    
