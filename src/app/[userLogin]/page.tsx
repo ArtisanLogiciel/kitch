9ba81e25f0c45e0bffcee6d28847e436bffe5d86
antonin michel <a.copiercoller@gmail.com>
2023-06-30 22:15:02 +0200

    mise en commentaire de code qui bug

--- src/app/[userLogin]/page.tsx
+++ src/app/[userLogin]/page.tsx
@@ -18,24 +18,24 @@ import Chat from "@/Components/Chat";
 //   timeSecondes: number;
 // }
 export default function Stream() {
-  const searchParams = useSearchParams();
-  const viewer: number = Number(searchParams.get("viewer"))
-  const timestamp = searchParams.get("time")
-  const timestampNb: number = Number(timestamp)
-
-  const timestampNow = Date.parse(new Date().toString())  // car le timestamp est en STRING (pourtant passé en NUMBER dans le LINK) !!!???
+  // const searchParams = useSearchParams();
+  // const viewer: number = Number(searchParams.get("viewer"))
+  // const timestamp = searchParams.get("time")
+  // const timestampNb: number = Number(timestamp)
+  const userLogin = useRouter().query.userLogin as string;
+  // const timestampNow = Date.parse(new Date().toString())  // car le timestamp est en STRING (pourtant passé en NUMBER dans le LINK) !!!???
 
   // On calcule le temps écoulé en secondes depuis le début de la vidéo
-  const timeSecondes = (timestampNow - timestampNb) / 1000
-  const router = useRouter();
-  const userLogin = router.query.userLogin as string;
-  const [activeChat, setActiveChat] = React.useState<boolean>(true);
+  // const timeSecondes = (timestampNow - timestampNb) / 1000
+  // const router = useRouter();
+  // const userLogin = router.query.userLogin as string;
+  // const [activeChat, setActiveChat] = React.useState<boolean>(true);
   // console.log("******activeChat******* : ",activeChat);
 
   return (
-    <div className={activeChat ? "grid grid-cols-[80%_20%] w-full h-full " : "grid grid-cols-[100%_0%] w-full h-full "} >
+    <div className={/*activeChat ? "grid grid-cols-[80%_20%] w-full h-full " : "grid grid-cols-[100%_0%] w-full h-full "*/""} >
         <Channel userLogin={userLogin} />
-        <div className={!activeChat ? "ml-5 " : ""}>{/*<Chat activeChat={activeChat} setActiveChat={setActiveChat} />*/}</div>
+        <div className={/*!activeChat ? "ml-5 " : ""*/ ""}>{/*<Chat activeChat={activeChat} setActiveChat={setActiveChat} />*/}</div>
     </div>
    
   );
