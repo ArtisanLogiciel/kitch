36a53cf3cb5eb2ad6c0d589c8b2172ba3d9440d0
Thibaut Barbiera <107733655+backpackoder@users.noreply.github.com>
2023-07-14 10:37:44 -0600

    Update commons.ts

--- src/commons/commons.ts
+++ src/commons/commons.ts
@@ -1,5 +1,6 @@
 export const URL = {
-  BASE: "http://localhost:3000",
+  // BASE: "http://localhost:3000",
+  BASE: "https://kitch-french.vercel.app/",
   API_TWITCH: "api/twitch",
   API_GAME: "api/game",
 };
