98e548104ead30ab0b4ecbeccd26bf3ed1137d17
antonin michel <a.copiercoller@gmail.com>
2023-07-15 19:39:21 +0200

    ajout du channel

--- src/app/[userLoginId]/page.tsx
+++ src/app/[userLoginId]/page.tsx
@@ -31,10 +31,10 @@ export default function Stream() {
   // const userLogin = router.query.userLogin as string;
   // const [activeChat, setActiveChat] = React.useState<boolean>(true);
   // console.log("******activeChat******* : ",activeChat);
-
+  const userLoginId = useRouter().query.userLoginId as string;
   return (
     <div className={/*activeChat ? "grid grid-cols-[80%_20%] w-full h-full " : "grid grid-cols-[100%_0%] w-full h-full "*/""} >
-        {/* <Channel userLogin={router} /> */}
+        <Channel userLogin={userLoginId} />
         <div className={/*!activeChat ? "ml-5 " : ""*/ ""}>{/*<Chat activeChat={activeChat} setActiveChat={setActiveChat} />*/}</div>
     </div>
    
